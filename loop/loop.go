55c70e6bae04176e02a46bfa3d82919d4be59930
YoussefWilliam <youssef.william997@gmail.com>
2021-07-30 13:28:24 +0200

    Adding if conditions

--- loop/loop.go
+++ loop/loop.go
@@ -13,4 +13,16 @@ func main() {
 	for index, value := range names {
 		fmt.Printf("The index is %v and the its value is %q \n", index, value)
 	}
+
+	age:= 23
+
+	if age > 20 {
+		fmt.Println("You are an adult")
+	}
+	if age > 22 {
+		fmt.Println("You just graduated college")
+	}
+	if age <= 23 {
+		fmt.Println("You will be great someday")
+	}
 }
